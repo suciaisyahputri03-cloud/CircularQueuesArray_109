03efe431e37aa627e6920ae41995f496698857fc
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-28 10:52:09 +0700

    Menambahkan pengecekan queue penuh dan kosong pada insert

--- circularQueuesarry.cpp
+++ circularQueuesarry.cpp
@@ -21,4 +21,20 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
 };
