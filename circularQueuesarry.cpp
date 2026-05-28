e572d5a0bfdb6b358bcb1345f1e51807cea6c78e
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-28 11:20:07 +0700

    Menambahkan pemanggilan fungsi insert remove display

--- circularQueuesarry.cpp
+++ circularQueuesarry.cpp
@@ -136,7 +136,30 @@ int main()
 
             switch (ch)
             {
-                
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }  
             }
         }
 
