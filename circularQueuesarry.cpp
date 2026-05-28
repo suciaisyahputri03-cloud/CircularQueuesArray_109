f0e4762e5be91d9c96736c77ba5112197887be5a
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-28 11:03:22 +0700

    Menambahkan tampilan isi queue dari front ke rear

--- circularQueuesarry.cpp
+++ circularQueuesarry.cpp
@@ -80,4 +80,18 @@ public:
             cout << "Queue is empty\n";
             return;
         }
+
+        cout << "\nElements in the queue are....\n";
+
+        // jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+        else
 };
